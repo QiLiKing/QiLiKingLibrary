d3a17d86156ea0be128e0509ddfd58e116cedb4e
ZhaoShuo <1055329812@qq.com>
2018-08-09 17:32:27 +0800

    first commit

---
+++ qlklib/src/main/java/com/qlk/lib/utils/FileUtil.java
@@ -0,0 +1,10 @@
+package com.qlk.lib.utils;
+
+/**
+ * <br/>
+ * QQ：1055329812<br/>
+ * Created by QiLiKing on 2018/8/9 17:23
+ */
+public class FileUtil {
+
+}
---
+++ qlklib/src/main/java/com/qlk/lib/utils/IOUtil.java
@@ -0,0 +1,16 @@
+package com.qlk.lib.utils;
+
+import java.io.Closeable;
+
+/**
+ * <br/>
+ * QQ：1055329812<br/>
+ * Created by QiLiKing on 2018/8/9 17:23
+ */
+public class IOUtil {
+    public static final void close(Closeable... streams) {
+        for (Closeable stream : streams) {
+
+        }
+    }
+}
